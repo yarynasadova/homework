65d28df589fe95a864e54c022885a9c2c62a2923
Yaryna <yaryna.sadova@gmail.com>
2019-05-30 16:35:23 +0000

    Added HTML Code

---
+++ index.js
@@ -0,0 +1,65 @@
+1.https://www.codewars.com/kata/training-js-number-2-basic-data-types-number/train/javascript
+
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+function equal1(){
+  var a=v1   
+  var b=v1   
+  return a+b;
+}
+//Please refer to the example above to complete the following functions
+function equal2(){
+  var a= v3 //set number value to a
+  var b= v1 //set number value to b
+  return a-b;
+}
+function equal3(){
+  var a=v1  //set number value to a
+  var b=v5 //set number value to b
+  return a*b;
+}
+function equal4(){
+  var a=v4  //set number value to a
+  var b=v5 //set number value to b
+  return a/b;
+}
+function equal5(){
+  var a=v6  //set number value to a
+  var b=v3 //set number value to b
+  return a%b;
+}
+
+2.https://www.codewars.com/kata/training-js-number-3-basic-data-types-string/train/javascript
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  return d1+a2+d2;
+}
+function Bee(){
+  //select some variable to combine "Bee"
+  return b1+e2+e2;
+}
+function banana(){
+  //select some variable to combine "banana"
+  return b2+a2+n2+a2+n2+a2;
+}
+
+//answer some questions if you finished works above
+function answer1(){
+  //the answer should be "yes" or "no"
+  return "yes";
+}
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer3(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+
+3. https://www.codewars.com/kata/training-js-number-1-create-your-first-js-function-and-print-helloworld/train/javascript
+
+function helloWorld(){
+  var somewords="Hello World!"
+  console.log(somewords)
+}
